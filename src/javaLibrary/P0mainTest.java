c63c5103ab397c4a1dc535e8fa175a0b6527e374
ruumu <zugogge2@yahoo.co.jp>
2016-01-25 21:47:54 +0900

    追加

--- src/javaLibrary/P0mainTest.java
+++ src/javaLibrary/P0mainTest.java
@@ -1,9 +1,20 @@
 package javaLibrary;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class P0mainTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String str = "abcdef";
+		String regex = "abc";
+		Pattern p = Pattern.compile(regex);
+
+		Matcher m = p.matcher(str);
+		if (m.find()){
+		  System.out.println("ƒ}ƒbƒ`‚µ‚Ü‚µ‚½");
+		  System.out.println(m.group());
+		}
 
 	}
 
--- src/javaLibrary/P2textConfig.java
+++ src/javaLibrary/P2textConfig.java
@@ -1,5 +1,34 @@
 package javaLibrary;
 
-public class P2textConfig {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class P2textConfig {
+	
+	//特定の文字列で区切った
+	public void inputFline(String readLine) {
+		readLine = readLine.trim();
+		String[] readLineArray = readLine.split(" ");
+//		a = Integer.parseInt(readLineArray[0]);
+//		b = Integer.parseInt(readLineArray[1]);
+//		R = Integer.parseInt(readLineArray[2]);
+	}
+
+	
+	//パターンマッチ
+	public void pm(){
+		
+		String str = "abcdef";
+		String regex = "abc";
+		Pattern p = Pattern.compile(regex);
+
+		Matcher m = p.matcher(str);
+		if (m.find()){
+		  System.out.println("マッチしました");
+		  System.out.println(m.group());
+		}
+		
+		
+	}
+	
 }
